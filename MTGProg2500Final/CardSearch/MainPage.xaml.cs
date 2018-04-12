f62eaf4eec764abc873fe9c0516357f3cb8e1f20
Kyle Bulger <w0217578@nscc.ca>
2018-04-12 13:01:19 -0300

    Set Button

--- MTGProg2500Final/CardSearch/MainPage.xaml.cs
+++ MTGProg2500Final/CardSearch/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace CardSearch
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(SetsPage));
+        }
     }
 }
