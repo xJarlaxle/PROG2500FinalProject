33ce297550944b74702114b5e958a08666f4cd3c
xJarlaxle <31731520+xJarlaxle@users.noreply.github.com>
2018-04-12 15:01:22 -0300

    Sets Page update

---
+++ MTGProg2500Final/CardSearch/SetSearchButtonClick.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CardSearch
+{
+    public class SetSearchButtonClick : ICommand {
+
+        SetPageData _spd;
+        
+        public event EventHandler CanExecuteChanged;
+        
+
+        public SetSearchButtonClick(SetPageData spd) {
+            this._spd = spd;
+        }
+
+        
+
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public async void Execute(object parameter) {
+            
+            var result = await _spd.controller.GetSetListAsync();
+        }
+
+
+
+    }
+}
