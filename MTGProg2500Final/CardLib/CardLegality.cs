6e7917dbf43bbc650cb74fae02eb8ef7de33be82
jwilliamson <w0298109@nscc.ca>
2018-04-12 13:22:41 -0300

    Model and controller push

---
+++ MTGProg2500Final/CardLib/CardLegality.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardLib
+{
+    public class Legality
+    {
+        public string format { get; set; }
+        public string legality { get; set; }
+    }
+}
